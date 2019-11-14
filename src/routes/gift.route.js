cbb864a27e875fca55ebe83a0a38004aff94c94e
alexzender45 <alexzendersamuel33@gmail.com>
2019-11-14 12:35:09 -0800

    updated gift controller

--- src/routes/gift.route.js
+++ src/routes/gift.route.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post('/gifts', Authentication.verify_token, multerUploads, Gift.add_gift);
 router.get('/gifts', Authentication.verify_token, Gift.get_all_gift_query);
 router.get('/gifts/:gift_id', Authentication.verify_token, Gift.get_gift_by_id);
-//router.put('/gifts/:gift_id', Authentication.verify_token, Gift.update_gift);
+router.put('/gifts/:gift_id', Authentication.verify_token, Gift.update_gift);
 router.delete('/gifts/:gift_id', Authentication.verify_token, Gift.delete_gift);
 
 export default router;
