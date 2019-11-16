85635d907a854cf1b4f28bec55ebd9bbc70a51e9
alexzender45 <alexzendersamuel33@gmail.com>
2019-11-15 20:39:00 -0800

    updated articles route

--- src/routes/article.route.js
+++ src/routes/article.route.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/articles', Authentication.verify_token, Article.add_article);
 router.get('/articles', Authentication.verify_token, Article.get_all_article_table);
 router.get('/articles/:article_id', Authentication.verify_token, Article.get_article_by_id);
-router.put('/articles/:article_id', Authentication.verify_token, Article.update_article);
+router.patch('/articles/:article_id', Authentication.verify_token, Article.update_article);
 router.delete('/articles/:article_id', Authentication.verify_token, Article.delete_article);
 
 export default router;
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,7 +4,7 @@ import User from '../controller/user.controller';
 
 const router = express.Router();
 
-router.post('/signup', User.create);
+router.post('/create_user', User.create);
 router.post('/signin', User.login);
 
 export default router;
